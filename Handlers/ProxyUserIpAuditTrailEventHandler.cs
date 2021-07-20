e0e8c8ba34196e92b7a4257a1f398e5f3f37ff5b
Krisztián Németh <krisztian.nemeth@lombiq.com>
2021-07-20 18:13:48 +0200

    Add proxy IP handler for AuditTrail create event

---
+++ Handlers/ProxyUserIpAuditTrailEventHandler.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Http;
+using OrchardCore.AuditTrail.Services;
+using OrchardCore.AuditTrail.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lombiq.AuditTrailExtensions.Handlers
+{
+    public class ProxyUserIpAuditTrailEventHandler : AuditTrailEventHandlerBase
+    {
+        private readonly IHttpContextAccessor _hca;
+
+        public ProxyUserIpAuditTrailEventHandler(IHttpContextAccessor hca) => _hca = hca;
+
+        public override Task CreateAsync(AuditTrailCreateContext context)
+        {
+            if (_hca?.HttpContext?.Request?.Headers.ContainsKey("X-Forwarded-For") == true)
+            {
+                context.ClientIpAddress = _hca?.HttpContext.Request.Headers["X-Forwarded-For"]
+                    .ToString()
+                    .Split(',')
+                    .FirstOrDefault()
+                    ?.Trim();
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,12 +1,17 @@
+using Lombiq.AuditTrailExtensions.Handlers;
 using Lombiq.AuditTrailExtensions.Services;
 using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.AuditTrail.Services;
 using OrchardCore.Modules;
 
 namespace OrchardCore.AuditTrail
 {
     public class Startup : StartupBase
     {
-        public override void ConfigureServices(IServiceCollection services) =>
+        public override void ConfigureServices(IServiceCollection services)
+        {
             services.AddScoped<IAuditTrailContentVersionNumberService, AuditTrailContentVersionNumberService>();
+            services.AddScoped<IAuditTrailEventHandler, ProxyUserIpAuditTrailEventHandler>();
+        }
     }
 }
