9f2ba91388e56d9d67b1ad408590bc21e0ed8f43
Dávid El-Saig <david.el-saig@lombiq.com>
2021-07-29 22:01:07 +0200

    Add AuditTrailCreateContext.AppendComment extension.

---
+++ Extensions/AuditTrailCreateContextExtensions.cs
@@ -0,0 +1,15 @@
+using static System.Environment;
+
+namespace OrchardCore.AuditTrail.Services.Models
+{
+    public static class AuditTrailCreateContextExtensions
+    {
+        public static void AppendComment(this AuditTrailCreateContext context, string newComment, string separator = null)
+        {
+            separator ??= NewLine + NewLine;
+            context.Comment = !string.IsNullOrEmpty(context.Comment)
+                ? context.Comment + separator + newComment
+                : newComment;
+        }
+    }
+}
