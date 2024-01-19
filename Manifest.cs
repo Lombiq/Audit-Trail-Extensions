7c83773ae4a377797ef28a56b3ff871a36b90b22
Roland Márkus <roland.markus@lombiq.com>
2024-01-19 14:05:53 +0100

    Addressing warnings.

--- Manifest.cs
+++ Manifest.cs
@@ -14,9 +14,9 @@ using static Lombiq.AuditTrailExtensions.Constants.FeatureIds;
     Name = "Lombiq Audit Trail Extensions",
     Category = "Content",
     Description = "Module with additional functionality to the stock Audit Trail.",
-    Dependencies = new[]
-    {
+    Dependencies =
+    [
         "OrchardCore.Contents",
         "OrchardCore.AuditTrail",
-    }
+    ]
 )]
